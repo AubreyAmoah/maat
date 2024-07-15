4b5c3f0d4cc04f3f5177bd6fa27cc1242c2e5405
AubreyAmoah <2gee419@gmail.com>
2024-07-15 01:06:11 +0000

    input sound done

--- speaker.js
+++ speaker.js
@@ -31,16 +31,23 @@ microphone.onclick = () => {
     synth.cancel();
   recognition.start();
   textToSpeech("Ready to receive a command.");
+  prompt.value = ""
 };
 
 recognition.onresult = (event) => {
   const word = event.results[0][0].transcript;
   prompt.value = word;
+  question.innerText = prompt.value;
   console.log(`Confidence: ${event.results[0][0].confidence}`);
 };
 
 recognition.onspeechend = () => {
   recognition.stop();
+  workspace.focus();
+  const statement = prompt.value;
+  const result = calculateFromStatement(statement);
+  answer.innerText = `Your answer is ${result}`;
+  textToSpeech(answer.innerText)
 };
 
 // recognition.onnomatch = (event) => {
