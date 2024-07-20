392f7e7727f9a44d105c76bf25ac2ac717cd10ae
AubreyAmoah <2gee419@gmail.com>
2024-07-20 23:17:04 +0000

    test

--- script/main.js
+++ script/main.js
@@ -158,13 +158,18 @@ const regex =
 
 const hint =
   (hints.innerHTML = `Try remember to use keywords like: ${keywordsHTML} to initiate chat.`);
-
+const loadingScreen = document.getElementById("loading-screen");
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
-  const loadingScreen = document.getElementById("loading-screen");
 
   setTimeout(() => {
     loadingScreen.style.display = "none";
+
+    if (prompt.value === "") {
+      question.innerText = "Any thing you type or say will show up here";
+    } else {
+      question.innerText = prompt.value;
+    }
 
     mic.onclick = () => {
       if (user) {
@@ -185,34 +190,36 @@ document.addEventListener("DOMContentLoaded", (event) => {
             const result = calculatePercentage(statement);
             answer.innerText = `Your answer is ${result}`;
             textToSpeech(answer.innerText);
-          } else if (statement.includes("change my name")) {
-            recognition.stop();
-            textToSpeech(`What is your new name?`, () => {
-              recognition.start();
-            });
-            recognition.onresult = (event) => {
-              const word = event.results[0][0].transcript;
-              window.localStorage.setItem("user", word);
-              textToSpeech(`Your new name is ${word}`, () => {
-                isBusy = false;
-              });
-            };
+          } 
+          // else if (statement.includes("change my name")) {
+          //   recognition.stop();
+          //   textToSpeech(`What is your new name?`, () => {
+          //     recognition.start();
+          //   });
+          //   recognition.onresult = (event) => {
+          //     const word = event.results[0][0].transcript;
+          //     window.localStorage.setItem("user", word);
+          //     textToSpeech(`Your new name is ${word}`, () => {
+          //       isBusy = false;
+          //     });
+          //   };
 
-            recognition.onspeechend = () => {
-              if (isBusy) {
-                recognition.start();
-              }
-            };
+          //   recognition.onspeechend = () => {
+          //     if (isBusy) {
+          //       recognition.start();
+          //     }
+          //   };
 
-            recognition.onerror = (event) => {
-              textToSpeech(
-                "I cannot understand you. Please give me a valid name",
-                () => {
-                  recognition.start();
-                }
-              );
-            };
-          } else {
+          //   recognition.onerror = (event) => {
+          //     textToSpeech(
+          //       "I cannot understand you. Please give me a valid name",
+          //       () => {
+          //         recognition.start();
+          //       }
+          //     );
+          //   };
+          // } 
+          else {
             const result = calculateFromStatement(statement);
             answer.innerText = `Your answer is ${result}`;
             textToSpeech(answer.innerText);
@@ -273,12 +280,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //   }, 2000);
     // }
 
-    if (prompt.value === "") {
-      question.innerText = "Any thing you type or say will show up here";
-    } else {
-      question.innerText = prompt.value;
-    }
-
     //prompt handler
     handleKeyUp(prompt, question);
     handleKeyDown(prompt, answer, workspace);
@@ -312,126 +313,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
       synth
     );
 
-    if (isBusy === false) {
-      hints.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(hints.textContent);
-        }
-      };
+    pitch.onchange = () => {
+      pitchValue.textContent = pitch.value;
+    };
 
-      hints.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      question.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(question.textContent);
-        }
-      };
-
-      question.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      answer.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(answer.textContent);
-        }
-      };
-
-      answer.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      workspace.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(
-            "This is your workspace. It contains your question and answer."
-          );
-        }
-      };
-
-      workspace.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      voiceSettingBtn.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(
-            "Settings button. Click here to change my voice or increase and decrease the speed of my voice"
-          );
-        }
-      };
-
-      voiceSettingBtn.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      magnifierBtn.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(
-            "Magnify button. Click here to toggle on or off the magnifying glass"
-          );
-        }
-      };
-
-      magnifierBtn.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      deafModeBtn.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech(
-            "Deaf mode button. Click here to enable or disable deaf mode. Remember, deaf mode turns off voice assistant."
-          );
-        }
-      };
-
-      deafModeBtn.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      microphone.onmouseenter = (e) => {
-        e.preventDefault();
-        synth.cancel();
-        if (DeafModeOff) {
-          textToSpeech("Click here to start speaking");
-        }
-      };
-
-      microphone.onmouseleave = (e) => {
-        e.preventDefault();
-        synth.cancel();
-      };
-
-      pitch.onchange = () => {
-        pitchValue.textContent = pitch.value;
-      };
-
-      rate.onchange = () => {
-        rateValue.textContent = rate.value;
-      };
-    }
+    rate.onchange = () => {
+      rateValue.textContent = rate.value;
+    };
   }, 5000);
 });
