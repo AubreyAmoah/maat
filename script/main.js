75b26ce805a1a94b8dd918dc0a2bb6b2b2c79a17
AubreyAmoah <2gee419@gmail.com>
2024-07-20 14:11:23 +0000

    test

--- script/main.js
+++ script/main.js
@@ -96,6 +96,27 @@ if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
+const textToSpeech = (element) => {
+  const utterThis = new SpeechSynthesisUtterance(element);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (const voice of voices) {
+    if (voice.name === selectedOption) {
+      utterThis.voice = voice;
+    }
+  }
+  utterThis.pitch = pitch.value;
+  utterThis.rate = rate.value;
+  synth.speak(utterThis);
+
+  utterThis.onpause = (event) => {
+    const char = event.utterance.text.charAt(event.charIndex);
+    console.log(
+      `Speech paused at character ${event.charIndex} of "${event.utterance.text}", which is "${char}".`
+    );
+  };
+};
+
 const regex =
   /\bsum\b|\bsum of\b|\bsumof\b|\bplus\b|\badd\b|\bminus\b|\bsubtract\b|[-]|\baddition\b|\bsubtraction\b|\bnegative\b|\bpositive\b|\bproduct\b|\bproduct of\b|\bproductof\b|[*]|\bmultiply\b|\bmultiplication\b|\btimes\b|\bmultiplied\b|\bdivision\b|\bdivide\b|\bdivided\b|[/]|\bfrom\b|\bremove\b/gi;
 
@@ -108,27 +129,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   setTimeout(() => {
     loadingScreen.style.display = "none";
-
-    const textToSpeech = (element) => {
-      const utterThis = new SpeechSynthesisUtterance(element);
-      const selectedOption =
-        voiceSelect.selectedOptions[0].getAttribute("data-name");
-      for (const voice of voices) {
-        if (voice.name === selectedOption) {
-          utterThis.voice = voice;
-        }
-      }
-      utterThis.pitch = pitch.value;
-      utterThis.rate = rate.value;
-      synth.speak(utterThis);
-
-      utterThis.onpause = (event) => {
-        const char = event.utterance.text.charAt(event.charIndex);
-        console.log(
-          `Speech paused at character ${event.charIndex} of "${event.utterance.text}", which is "${char}".`
-        );
-      };
-    };
 
     const readHints = () => {
       textToSpeech(hint);
